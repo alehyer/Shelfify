4bff5c251f00495aa4bcb3089a582420cdc2f0af
BobyJeff <151848178+BobyJeff@users.noreply.github.com>
2024-11-05 22:38:34 +0800

    Added js to the login page

--- src/login.js
+++ src/login.js
@@ -1,6 +1,28 @@
 document.addEventListener('DOMContentLoaded', function() {
-    document.getElementById("login-helpInfo").addEventListener('click', function() {
+    document.getElementById('login-helpInfo').addEventListener('click', function() {
         alert("Please contact IT Services \n\nPhone Number: +601234567890 \nEmail: shelfify@customersupport.com");
+    });
+
+    document.getElementById('login-button').addEventListener('click', function() {
+        const idElement = document.getElementById('librarian-id');
+        const passwordElement = document.getElementById('librarian-password');
+        
+        const id = idElement.value.trim();
+        let password = passwordElement.value.trim();
+    
+        if (id === "" || password === "") {
+            alert("Please fill in all required fields.");
+            return;
+        }
+        
+        // admin account
+        if (id === "123" && password === "abc") {
+            window.location.href = `index.html`;
+        } else {
+            alert("Incorrect ID or password.");
+        }
+
+        passwordElement.value = "";
     });
 });
 
