d65228229c364eeef7904ce6a8292d256fea9c6b
BobyJeff <151848178+BobyJeff@users.noreply.github.com>
2024-11-05 22:05:24 +0800

    login page (first draft)

---
+++ src/login.js
@@ -0,0 +1,6 @@
+document.addEventListener('DOMContentLoaded', function() {
+    document.getElementById("login-helpInfo").addEventListener('click', function() {
+        alert("Please contact IT Services \n\nPhone Number: +601234567890 \nEmail: shelfify@customersupport.com");
+    });
+});
+
